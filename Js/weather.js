f46bb83138b3fac766383b5f7e77a8545449de45
PRASANNA KUMAR BEZAVADA <bprasanna.555@gmail.com>
2018-07-23 09:01:51 -0700

    sixth commit

--- Js/weather.js
+++ Js/weather.js
@@ -21,7 +21,7 @@ $.getJSON(api,function(data){
     celsiTemp = (kelvin-273).toFixed(1);
     
     console.log(city);
-    $("#city").html(api);
+    $("#city").html(city);
     $("#weatherType").html(weatherType);
     $("#fariTemp").html(fariTemp + " &#8457;");
      $("#fariTemp").click(function(){
@@ -42,7 +42,7 @@ $.getJSON(api,function(data){
     if(fariTemp > 80){
        $('body').css('background-image','url(https://images.unsplash.com/photo-1419833173245-f59e1b93f9ee?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c3173da6dc330f4f4073b411732825f0&auto=format&fit=crop&w=750&q=80)'); 
     }
-    else if(fariTemp > 70){
+    else if(fariTemp > 60){
         $('body').css('background-image','url(https://images.unsplash.com/photo-1428592953211-077101b2021b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ca4c0c8dc291ec21f61ba16256362474&auto=format&fit=crop&w=667&q=80)'); 
         
     }
